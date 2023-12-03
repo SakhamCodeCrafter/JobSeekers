aed214f9140f465ca9a5bd67d71999b1983184e3
Saksham Walia <wsaksham37@gmail.com>
2023-12-03 19:54:13 +0530

    edited

--- client/src/components/Employee/EmployeeNavbar.jsx
+++ client/src/components/Employee/EmployeeNavbar.jsx
@@ -1,14 +1,8 @@
-import React, { useState, useEffect } from "react";
-import { GiHamburgerMenu } from "react-icons/gi";
+import React from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 const EmployeeNavbar = () => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 1000);
-  const [toggle, setToggle] = useState(false);
-  const navigate = useNavigate();
-
   const handleLogout = async () => {
     try {
       const response = await axios.get("/logout");
@@ -21,94 +15,41 @@ const EmployeeNavbar = () => {
     }
   };
 
-  useEffect(() => {
-    const handleResize = () => {
-      const width = window.innerWidth;
-      setIsMobile(width < 1000);
-      if (width > 768) {
-        setToggle(false);
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
-    <div className={`grid grid-cols-2 justify-between px-4 py-3 bg-slate-300`}>
+    <div
+      className={`w-full grid grid-cols-2 justify-between px-1 py-1 bg-slate-200`}
+    >
       <div>
         <img
           src="https://www.jobseeker.com/opengraph/image.png"
-          className="w-[10%]"
-        ></img>
+          className="w-[41%] md:w-[18%]"
+          alt="Logo"
+        />
       </div>
-      <div
-        className={`flex gap-4 justify-end ${
-          isMobile ? "w-full  flex mt-4" : "w-auto"
-        }`}
-      >
-        {!isMobile && (
-          <ul className="flex mt-2 gap-4 items-center justify-center">
-            <Link to={"/home"} className="linkStyle">
-              <li className="font-semibold text-md">Home</li>
-            </Link>
-            <Link to={"/about"} className="linkStyle">
-              <li className="font-semibold text-md">About</li>
-            </Link>
-           
-          </ul>
-        )}
-        {isMobile && (
-          <div className="flex w-full justify-end -mt-[5rem]">
-            <GiHamburgerMenu
-              className="inline-block top-0 text-3xl"
-              onClick={() => {
-                setToggle(!toggle);
-              }}
-            />
-          </div>
-        )}
-        <div className="hidden md:flex">
-          <button
-            onClick={handleLogout}
-            className="bg-blue-600 px-4 text-lg text-white"
-          >
-            Logout
-          </button>
-        </div>
-      </div>
-      {isMobile && toggle && (
-        <ul className="flex flex-col gap-4 bg-gray-300 py-3 w-full">
-          <Link to={"/main"}>
-            <li className="font-semibold  text-md">Home</li>
-          </Link>
-          <Link to={"/about"}>
-            <li className="font-semibold  text-md">About</li>
-          </Link>
-          <li className="font-semibold  text-md">Careers</li>
+      <div className="flex justify-end align-middle mt-1 lg:mt-[15px]">
+        <div className="grid grid-cols-3">
+          <div className="col-span-2 flex justify-end gap-4 mt-2.5 mr-5 lg:mr-5 lg:mt-4">
+            <div className="font-bold text-[8px] md:text-[14px]">
+              <Link to="/home">Home</Link>
+            </div>
 
-          <div
-            className={`flex gap-2 ${
-              isMobile ? "w-full justify-between flex mt-4" : "w-auto"
-            }`}
-          >
-            <div className=" w-full justify-end items-center">
-              {toggle && (
-                <div className=" space-x-5">
-                  <button
-                    onClick={handleLogout}
-                    className="bg-blue-600 py-2 px-4 text-lg text-white"
-                  >
-                    Logout
-                  </button>
-                </div>
-              )}
+            <div className="font-bold text-[8px] md:text-[14px]">
+              <Link to="/about">About</Link>
             </div>
           </div>
-        </ul>
-      )}
+          <div>
+            <button
+              onClick={handleLogout}
+              class="inline-flex items-center justify-center lg:mt-2  px-3 py-1 md:px-4 md:py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-blue-600 border border-blue-700 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              data-rounded="rounded-md"
+              data-primary="blue-600"
+              data-primary-reset="{}"
+            >
+              <p className="text-[8px] md:text-[14px]">LogOut</p>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- client/src/components/Employer/EmployerNavbar.jsx
+++ client/src/components/Employer/EmployerNavbar.jsx
@@ -1,14 +1,8 @@
-import React, { useState, useEffect } from "react";
-import { GiHamburgerMenu } from "react-icons/gi";
+import React from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 const EmployerNavbar = () => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 1000);
-  const [toggle, setToggle] = useState(false);
-  const navigate = useNavigate();
-
   const handleLogout = async () => {
     try {
       const response = await axios.get("/logoutEmp");
@@ -21,21 +15,6 @@ const EmployerNavbar = () => {
     }
   };
 
-  useEffect(() => {
-    const handleResize = () => {
-      const width = window.innerWidth;
-      setIsMobile(width < 1000);
-      if (width > 768) {
-        setToggle(false);
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
     <div
       className={`w-full grid grid-cols-2 justify-between px-1 py-1 bg-slate-200 mb-10`}
@@ -43,16 +22,20 @@ const EmployerNavbar = () => {
       <div>
         <img
           src="https://www.jobseeker.com/opengraph/image.png"
-          className="w-[41%] md:w-[21%]"
+          className="w-[41%] md:w-[18%]"
           alt="Logo"
         />
       </div>
       <div className="flex justify-end align-middle mt-1 lg:mt-[15px]">
         <div className="grid grid-cols-3">
           <div className="col-span-2 flex justify-end gap-4 mt-2.5 mr-5 lg:mr-5 lg:mt-4">
-            <div className="font-bold text-[8px] md:text-[14px]"><Link to="/emphome">Home</Link></div>
+            <div className="font-bold text-[8px] md:text-[14px]">
+              <Link to="/emphome">Home</Link>
+            </div>
 
-            <div className="font-bold text-[8px] md:text-[14px]"><Link to="/empAbout">About</Link></div>
+            <div className="font-bold text-[8px] md:text-[14px]">
+              <Link to="/empAbout">About</Link>
+            </div>
           </div>
           <div>
             <button
@@ -63,7 +46,6 @@ const EmployerNavbar = () => {
               data-primary-reset="{}"
             >
               <p className="text-[8px] md:text-[14px]">LogOut</p>
-              
             </button>
           </div>
         </div>
